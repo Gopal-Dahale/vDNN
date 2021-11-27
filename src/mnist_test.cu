a753db681bce495ee6aeb62898c68f15193e4cf0
Dhruv88 <55611035+Dhruv88@users.noreply.github.com>
2021-11-27 11:13:45 +0530

    Update mnist_test.cu

--- src/mnist_test.cu
+++ src/mnist_test.cu
@@ -16,7 +16,7 @@ using namespace std;
 
 typedef unsigned char uchar;
 
-int num_train = 1000, num_test = 500;
+int num_train = 1500, num_test = 500;
 
 int reverseInt(int n) {
 	int bytes = 4;
@@ -480,13 +480,13 @@ int main() {
     layer_specifier.push_back(temp);
   }
 
-	int batch_size = 64;
+	int batch_size = 128;
 	long long dropout_seed = 1;
 	float softmax_eps = 1e-8;
 	float init_std_dev = 0.01;
 	NeuralNet net(layer_specifier, DATA_FLOAT, batch_size, TENSOR_NCHW, dropout_seed, softmax_eps, init_std_dev, vDNN_ALL, vDNN_MEMORY_OPTIMAL, SGD);
 
-	int num_epoch = 100;
+	int num_epoch = 15;
 	double learning_rate = 1e-3;
 	double learning_rate_decay = 0.9;
 	
